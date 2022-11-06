b513b6ce1fb777bcabbbe743e9c8db5591993dc0
Karthikeyan Bhargavan <karthik.bhargavan@gmail.com>
2022-11-06 13:52:28 +0100

    working around crate::something::*

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,9 @@
 mod test_tls13traces_internal;
 mod tls13formats;
 mod tls13handshake;
-mod tls13record;
+
+pub mod tls13record;
+pub use tls13record::*;
 
 pub mod tls13utils;
 pub use tls13utils::*;
